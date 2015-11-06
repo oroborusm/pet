e1ec706ea8fc4dff941fb395afe08f7a522d2c0b
oroborusm <juan.bustamante.dg@gmail.com>
2015-11-05 23:54:29 -0300

    petmania v 1.6.1.2, css ordenado

---
+++ js/miscript.js
@@ -0,0 +1,141 @@
+$(function(){
+
+	$('#homeslider').bxSlider({
+		controls: false,
+		auto: true
+	});
+
+	var i = 0;
+	$('.contcomment').slimScroll({
+		height: '275px'
+	});
+
+	$('.añadeCarrituHome').on('click', function(){
+		setTimeout(function() {
+			$('.cross').trigger('click');
+		}, 5000);
+	});
+
+	$('.sumaComentario').on('click', function(){
+		$('.agregaComentario').toggleClass('activo')
+	});
+
+
+    $(".ancla").click(function(){
+		vinculo = $(this).attr("href")
+		destino = $(vinculo).offset().top - 100;    
+		$("html:not(:animated), body:not(:animated)").animate({scrollTop:destino}, 800)
+			return false
+	});
+    var removeClass = true;
+
+	$('.carrito').on('click', function(){
+		$('.carritoHover').toggleClass('activo')
+		removeClass = false
+	})
+
+	$('.login').on('click', function(){
+		$('.accedeCuentaChica').toggleClass('activo')
+		removeClass = false
+	});
+
+	$('.nombre').on('click', function(){
+		$('.despliega').addClass('activo')
+		removeClass = false
+	});
+
+	$('#cierraDespliega').on('click', function(){
+		$('.carritoHover').removeClass('activo')
+		$('.despliega').removeClass('activo')
+	});
+
+	$("html").on('click',function () {
+		if (removeClass) {
+			$('.carritoHover').removeClass('activo');
+			//$('.accedeCuenta').removeClass('activo');
+			$('.despliega').removeClass('activo');
+		}
+		removeClass = true;
+	});
+    
+    var nav = $('.nav-container');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 136) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+    });
+
+	/*defineSizes();
+	$(window).on("resize", defineSizes);
+	function defineSizes(){
+		$(".categoriasHome .contenedor, #htmlcontent_home").each(function(i,el){
+			$(el).css({
+				"height": ($(".contenedor, #htmlcontent_home").width() * 0.75)+"px"
+			});
+		});
+	}*/
+
+	$('.menuTop > li:has(> ul)').addClass('item-has-children')
+
+	$('.item-has-children > ul').addClass('sub-menu')
+	$('.item-has-children > ul').addClass('sub-menuDesktop')
+
+	$('#subcategories').addClass('subcategories')
+
+	if($(window).width() < 800){
+
+		$('.sub-menuDesktop > li:has(> ul)').addClass('item-has-children')
+		$('.sub-menuDesktop > li > ul').addClass('sub-menu')
+	}
+		
+	$(".icono").each(function(i){
+		i++;
+		$(this).addClass("ico"+i);
+	});	
+
+	$(".menuTop > li").each(function(i){
+		i++;
+		$(this).addClass("maniaBackground"+i);
+	});	
+	$('.submenu-container > li').removeClass("listaMenuTop");
+
+
+	if($(window).width() > 800){
+		$('.menuTop > li:has(> ul)').addClass('hoverMenu')
+	}
+
+	$('.owl-carousel').owlCarousel({
+		autoplay:true,
+		autoplayHoverPause:true,
+		autoplayTimeout:3000,
+		loop:true,
+		margin:10,
+		nav:false,
+		pagination:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	})
+
+	$('input, textarea').placeholder();
+
+	$('.dropdown-button').dropdown({
+		inDuration: 300,
+		outDuration: 225,
+		constrain_width: false, // Does not change width of dropdown to that of the activator
+		hover: true, // Activate on hover
+		gutter: 0, // Spacing from edge
+		belowOrigin: false // Displays dropdown below the button
+		}
+	);
+});
---
+++ modules/blocktopmenu/blocktopmenu.tpl
@@ -0,0 +1,11 @@
+<!-- manias -->
+{if $MENU != ''}
+	<!-- Menu -->
+	
+	<nav class="menuManias" id="cd-lateral-nav">
+		<ul class="cd-navigation menuTop">
+			{$MENU}
+		</ul>
+	</nav>
+	<!--/ Menu -->
+{/if}
